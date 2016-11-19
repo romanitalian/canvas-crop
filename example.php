a96f04027ca85455949b7bf0380d14e5e30cda62
romanitalian <romanitalian.net@gmail.com>
2016-11-19 20:56:31 +0300

    fix const in example script

--- example.php
+++ example.php
@@ -5,13 +5,13 @@ require_once __DIR__ . '/autoload.php';
 $CanvasCrop = new CanvasCrop();
 
 $CanvasCrop->loadImage('original1.png');
-$CanvasCrop->cropBySize(100, 100, ccBOTTOMRIGHT);
+$CanvasCrop->cropBySize(100, 100, CanvasCrop::ccBOTTOMRIGHT);
 $CanvasCrop->saveImage('final1.png');
 
 $CanvasCrop->flushImages();
 
 $CanvasCrop->loadImage('original2.png');
-$CanvasCrop->cropByPercent(15, 50, ccCENTER);
+$CanvasCrop->cropByPercent(15, 50, CanvasCrop::ccCENTER);
 $CanvasCrop->saveImage('final2.jpg', 90);
 
 $CanvasCrop->flushImages();
